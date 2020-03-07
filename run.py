8153e7503909da1debf73be99eb28259c28c3b0c
Cyril Geortay <c.geortay@student.uliege.be>
2020-03-07 10:36:30 +0100

    plotted figures for medium uniform traffic

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     nb_actions = 2  # Either stay at current phase or switch to the next one
     nb_episodes = 300
     nb_episode_steps = 3000
-    detection_rate = 0.5  # Percentage of vehicles that can be detected by the algorithm
+    detection_rate = 1.0  # Percentage of vehicles that can be detected by the algorithm
     gui = False
     alpha = 0.0001
     gamma = 0.9
@@ -46,10 +46,10 @@ if __name__ == "__main__":
     target_update_frequency = 3000
     hour_of_the_day = 8
     # Probability for a car to be generated on a particular route at a certain step
-    route_probabilities = [1. / 45] * 12
-    file_name = "model_50_medium.pt"
-    stats_file_name = "model_50_medium"
-    figure_name = "medium_uni_50"
+    route_probabilities = [1. / 30] * 3 + [1. / 60] * 3 + [1. / 30] * 3 + [1. / 60] * 3
+    file_name = "model_hor_100_medium.pt"
+    stats_file_name = "model_hor_100_medium"
+    figure_name = "medium_hor_100"
 
     # Initializing the simulator, agent and replay buffer
     agent = Agent(alpha, gamma, epsilon, epsilon_end, decay_steps, batch_size, nb_inputs, nb_actions, mem_size,
--- testing.py
+++ testing.py
@@ -3,7 +3,7 @@ import pickle
 import statistics
 
 if __name__ == "__main__":
-    simulator = sim.Simulator(30, 3000, 1.0, [1. / 30] * 12, 8, False)  # /!\ nb_episodes = 1500
+    simulator = sim.Simulator(30, 3000, 1.0, [1. / 30] * 3 + [1. / 60] * 3 + [1. / 30] * 3 + [1. / 60] * 3, 8, False)
     while simulator.step(None):
         '''if simulator.get_curr_nb_iterations() < 50:
             print("STEP " + str(simulator.get_curr_nb_iterations()))
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     print("Average reward:", str(reward))
     print("Average waiting time:", str(waiting_time))
 
-    with open("uniform1over30_baseline_r.txt", "wb") as file:
+    with open("data/hor1over30_ver1over60_baseline_r.txt", "wb") as file:
         pickle.dump(reward, file)
-    with open("uniform1over30_baseline_w.txt", "wb") as file:
+    with open("data/hor1over30_ver1over60_baseline_w.txt", "wb") as file:
         pickle.dump(waiting_time, file)
