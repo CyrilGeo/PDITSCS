1dd8333479938d085f801d8ca5854c6072155af4
CyrilGeo <c.geortay@student.uliege.be>
2020-03-26 20:08:37 +0100

    learning results

--- run.py
+++ run.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     nb_episodes_test = 30
     nb_episodes_between_tests = 5
     nb_episode_steps = 3000
-    detection_rate = 0.2  # Percentage of vehicles that can be detected by the algorithm
+    detection_rate = 0.5  # Percentage of vehicles that can be detected by the algorithm
     gui = False
     alpha = 0.0001
     gamma = 0.9
@@ -78,8 +78,8 @@ if __name__ == "__main__":
     target_update_frequency = 3000
     hour_of_the_day = 8
     # Probability for a car to be generated on a particular route at a certain step
-    route_probabilities = [1. / 30] * 3 + [1. / 45] * 3 + [1. / 30] * 3 + [1. / 45] * 3
-    gen_name = "model_hor_30_45_20"
+    route_probabilities = [1. / 30] * 12
+    gen_name = "model_50_high"
     file_name = gen_name + ".pt"
 
     print("LEARNING " + gen_name)
