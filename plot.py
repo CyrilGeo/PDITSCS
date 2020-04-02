e10737d8a423b40012c8702ecfeb76cabaa40973
Cyril Geortay <c.geortay@student.uliege.be>
2020-04-02 18:16:43 +0200

    plotting changes

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pickle
 import tensorflow as tf
+import numpy as np
 
 if __name__ == "__main__":
     '''baseline_name = "hor1over30_ver1over60"
@@ -35,66 +36,122 @@ if __name__ == "__main__":
     waiting_times1 = []
     waiting_times2 = []
     waiting_times3 = []
+    rewards1_dev = []
+    rewards2_dev = []
+    rewards3_dev = []
+    waiting_times1_dev = []
+    waiting_times2_dev = []
+    waiting_times3_dev = []
     baseline_r = 0
     baseline_w = 0
+    baseline_r_dev = 0
+    baseline_w_dev = 0
+
+    figure_name = "horizontal_boltz_30_60"
 
     # 100% detection rate
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/model_100_low/events.out.tfevents.1585155365.PC-CYRIL-LINUX.6284.0"):
+            "runs/model_boltz_hor_30_60_100/events.out.tfevents.1585431026.alan-compute-04.12904.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 episodes.append(event.step)
                 rewards1.append(value.simple_value)
             elif value.tag == "Average_waiting_time":
                 waiting_times1.append(value.simple_value)
+            elif value.tag == "Reward_standard_deviation":
+                rewards1_dev.append(value.simple_value)
+            elif value.tag == "Waiting_time_standard_deviation":
+                waiting_times1_dev.append(value.simple_value)
 
     # 50% detection rate
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/model_50_low/events.out.tfevents.1585168598.alan-compute-08.217244.0"):
+            "runs/model_boltz_hor_30_60_50/events.out.tfevents.1585458896.alan-compute-02.3965.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 rewards2.append(value.simple_value)
             elif value.tag == "Average_waiting_time":
                 waiting_times2.append(value.simple_value)
+            elif value.tag == "Reward_standard_deviation":
+                rewards2_dev.append(value.simple_value)
+            elif value.tag == "Waiting_time_standard_deviation":
+                waiting_times2_dev.append(value.simple_value)
 
     # 20% detection rate
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/model_20_low/events.out.tfevents.1585168567.alan-compute-07.215803.0"):
+            "runs/model_boltz_hor_30_60_20/events.out.tfevents.1585458910.alan-compute-02.4000.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 rewards3.append(value.simple_value)
             elif value.tag == "Average_waiting_time":
                 waiting_times3.append(value.simple_value)
+            elif value.tag == "Reward_standard_deviation":
+                rewards3_dev.append(value.simple_value)
+            elif value.tag == "Waiting_time_standard_deviation":
+                waiting_times3_dev.append(value.simple_value)
 
     # baseline
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/uniform_1over60_baseline/events.out.tfevents.1585171350.PC-CYRIL-LINUX.11892.0"):
+            "runs/hor1over30_ver1over60_baseline/events.out.tfevents.1585228860.PC-CYRIL-LINUX.25968.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 baseline_r = value.simple_value
             elif value.tag == "Average_waiting_time":
                 baseline_w = value.simple_value
+            elif value.tag == "Reward_standard_deviation":
+                baseline_r_dev = value.simple_value
+            elif value.tag == "Waiting_time_standard_deviation":
+                baseline_w_dev = value.simple_value
 
     plt.figure()
+    plt.grid()
     plt.plot(episodes, rewards1, color="limegreen", label="100% detection rate")
+    plt.errorbar(episodes, rewards1, yerr=rewards1_dev, color="limegreen", elinewidth=3, alpha=0.4)
     plt.plot(episodes, rewards2, color="steelblue", label="50% detection rate")
+    plt.errorbar(episodes, rewards2, yerr=rewards2_dev, color="steelblue", elinewidth=3, alpha=0.4)
     plt.plot(episodes, rewards3, color="gold", label="20% detection rate")
+    plt.errorbar(episodes, rewards3, yerr=rewards3_dev, color="gold", elinewidth=3, alpha=0.4)
     plt.axhline(y=baseline_r, color="r", label="fixed time (10s)")
-    plt.ylim(bottom=-0.6)
     plt.xlabel("Episode")
     plt.ylabel("Average reward")
     plt.legend()
-    # plt.savefig("figures/" + figure_name + "_r.png")
+    plt.savefig("figures/" + figure_name + "_r.png")
     plt.show()
 
     plt.figure()
+    plt.grid()
     plt.plot(episodes, waiting_times1, color="limegreen", label="100% detection rate")
+    plt.errorbar(episodes, waiting_times1, yerr=waiting_times1_dev, color="limegreen", elinewidth=3, alpha=0.4)
     plt.plot(episodes, waiting_times2, color="steelblue", label="50% detection rate")
+    plt.errorbar(episodes, waiting_times2, yerr=waiting_times2_dev, color="steelblue", elinewidth=3, alpha=0.4)
     plt.plot(episodes, waiting_times3, color="gold", label="20% detection rate")
+    plt.errorbar(episodes, waiting_times3, yerr=waiting_times3_dev, color="gold", elinewidth=3, alpha=0.4)
     plt.axhline(y=baseline_w, color="r", label="fixed time (10s)")
-    plt.ylim(bottom=1.0, top=8.0)
     plt.xlabel("Episode")
     plt.ylabel("Average waiting time (s)")
     plt.legend()
-    # plt.savefig("figures/" + figure_name + "_w.png")
+    plt.savefig("figures/" + figure_name + "_w.png")
     plt.show()
+
+    '''plt.figure()
+    plt.plot(episodes, rewards1_dev, color="limegreen", label="100% detection rate")
+    plt.plot(episodes, rewards2_dev, color="steelblue", label="50% detection rate")
+    plt.plot(episodes, rewards3_dev, color="gold", label="20% detection rate")
+    plt.axhline(y=baseline_r_dev, color="r", label="fixed time (10s)")
+    plt.ylim(bottom=0.01, top=0.08)
+    plt.xlabel("Episode")
+    plt.ylabel("Reward standard deviation")
+    plt.legend()
+    plt.savefig("figures/" + figure_name + "_r_dev.png")
+    plt.show()
+
+    plt.figure()
+    plt.plot(episodes, waiting_times1_dev, color="limegreen", label="100% detection rate")
+    plt.plot(episodes, waiting_times2_dev, color="steelblue", label="50% detection rate")
+    plt.plot(episodes, waiting_times3_dev, color="gold", label="20% detection rate")
+    plt.axhline(y=baseline_w_dev, color="r", label="fixed time (10s)")
+    plt.ylim(bottom=0.0, top=8.0)
+    plt.xlabel("Episode")
+    plt.ylabel("Waiting time standard deviation")
+    plt.legend()
+    plt.savefig("figures/" + figure_name + "_w_dev.png")
+    plt.show()'''
--- real_testing.py
+++ real_testing.py
@@ -1,7 +1,7 @@
 import lux_sim as sim
 
 if __name__ == "__main__":
-    simulator = sim.LuxSim()
+    simulator = sim.LuxSim(True)
     simulator.start_sim()
     while simulator.step():
         pass
