43a6852597caa6424173aa6cc7271b0648b6fdda
CyrilGeo <c.geortay@student.uliege.be>
2020-04-05 21:12:28 +0200

    learning results

--- submit.sh
+++ submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=SIMP_INT
+#SBATCH --job-name=REAL_INT
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=1-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --mem-per-cpu=10g
 #SBATCH --partition=all
 #SBATCH --gres=gpu:1
 
 export SUMO_HOME="/home/cgeortay/master_thesis/sumo/sumo"
-python3 run8.py
+python3 run11.py
