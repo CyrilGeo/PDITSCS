waiting time calculation fixed for real cases

plot.py
@@ -51,11 +51,11 @@ if __name__ == "__main__":
     baseline_adapted_r_dev = 0
     baseline_adapted_w_dev = 0
 
-    figure_name = "horizontal_45_60_zoomed"
+    figure_name = "LuST/realflow"
 
     # 100% detection rate
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/model_hor_45_60_100/events.out.tfevents.1585228331.alan-compute-07.189313.0"):
+            "runs/model_100_realflow/events.out.tfevents.1586286017.alan-compute-01.24456.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 episodes.append(event.step)
@@ -69,7 +69,7 @@ if __name__ == "__main__":
 
     # 50% detection rate
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/model_hor_45_60_50/events.out.tfevents.1585228364.alan-compute-08.44301.0"):
+            "runs/model_50_realflow/events.out.tfevents.1586287061.alan-compute-03.28743.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 rewards2.append(value.simple_value)
@@ -82,7 +82,7 @@ if __name__ == "__main__":
 
     # 20% detection rate
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/model_hor_45_60_20/events.out.tfevents.1585228388.alan-compute-08.44614.0"):
+            "runs/model_20_realflow/events.out.tfevents.1586287507.alan-compute-01.3235.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 rewards3.append(value.simple_value)
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 
     # baseline
     for event in tf.compat.v1.train.summary_iterator(
-            "runs/hor1over45_ver1over60_baseline/events.out.tfevents.1585228968.PC-CYRIL-LINUX.26038.0"):
+            "runs/realflow_baseline/events.out.tfevents.1586016169.PC-CYRIL-LINUX.14847.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
                 baseline_r = value.simple_value
@@ -107,7 +107,7 @@ if __name__ == "__main__":
                 baseline_w_dev = value.simple_value
 
     # baseline adapted
-    for event in tf.compat.v1.train.summary_iterator(
+    '''for event in tf.compat.v1.train.summary_iterator(
             "runs/hor1over45_ver1over60_adapted/events.out.tfevents.1585918655.PC-CYRIL-LINUX.7236.0"):
         for value in event.summary.value:
             if value.tag == "Average_reward":
@@ -117,35 +117,35 @@ if __name__ == "__main__":
             elif value.tag == "Reward_standard_deviation":
                 baseline_adapted_r_dev = value.simple_value
             elif value.tag == "Waiting_time_standard_deviation":
-                baseline_adapted_w_dev = value.simple_value
+                baseline_adapted_w_dev = value.simple_value'''
 
-    '''plt.figure()
+    plt.figure()
     plt.grid()
     plt.plot(episodes, rewards1, color="limegreen", label="100% detection rate")
-    plt.errorbar(episodes, rewards1, yerr=rewards1_dev, color="limegreen", elinewidth=1.5, alpha=0.4)
+    plt.errorbar(episodes, rewards1, yerr=rewards1_dev, color="limegreen", elinewidth=3, alpha=0.4)
     plt.plot(episodes, rewards2, color="steelblue", label="50% detection rate")
-    plt.errorbar(episodes, rewards2, yerr=rewards2_dev, color="steelblue", elinewidth=1.5, alpha=0.4)
+    plt.errorbar(episodes, rewards2, yerr=rewards2_dev, color="steelblue", elinewidth=3, alpha=0.4)
     plt.plot(episodes, rewards3, color="gold", label="20% detection rate")
-    plt.errorbar(episodes, rewards3, yerr=rewards3_dev, color="gold", elinewidth=1.5, alpha=0.4)
+    plt.errorbar(episodes, rewards3, yerr=rewards3_dev, color="gold", elinewidth=3, alpha=0.4)
     plt.axhline(y=baseline_r, color="r", label="fixed time (10s)")
     # plt.axhline(y=baseline_adapted_r, color="darkviolet", label="adapted fixed time")
     plt.xlabel("Episode")
     plt.ylabel("Average reward")
     plt.legend()
     plt.savefig("figures/" + figure_name + "_r.png")
-    plt.show()'''
+    plt.show()
 
     plt.figure()
     plt.grid()
     plt.plot(episodes, waiting_times1, color="limegreen", label="100% detection rate")
-    plt.errorbar(episodes, waiting_times1, yerr=waiting_times1_dev, color="limegreen", elinewidth=1.5, alpha=0.4)
+    plt.errorbar(episodes, waiting_times1, yerr=waiting_times1_dev, color="limegreen", elinewidth=3, alpha=0.4)
     plt.plot(episodes, waiting_times2, color="steelblue", label="50% detection rate")
-    plt.errorbar(episodes, waiting_times2, yerr=waiting_times2_dev, color="steelblue", elinewidth=1.5, alpha=0.4)
+    plt.errorbar(episodes, waiting_times2, yerr=waiting_times2_dev, color="steelblue", elinewidth=3, alpha=0.4)
     plt.plot(episodes, waiting_times3, color="gold", label="20% detection rate")
-    plt.errorbar(episodes, waiting_times3, yerr=waiting_times3_dev, color="gold", elinewidth=1.5, alpha=0.4)
+    plt.errorbar(episodes, waiting_times3, yerr=waiting_times3_dev, color="gold", elinewidth=3, alpha=0.4)
     plt.axhline(y=baseline_w, color="r", label="fixed time (10s)")
-    plt.axhline(y=baseline_adapted_w, color="darkviolet", label="adapted fixed time")
-    plt.ylim(bottom=2, top=7)
+    # plt.axhline(y=baseline_adapted_w, color="darkviolet", label="adapted fixed time")
+    # plt.ylim(bottom=2, top=7)
     plt.xlabel("Episode")
     plt.ylabel("Average waiting time (s)")
     plt.legend()

real_testing.py
@@ -1,13 +1,14 @@
-# import lux_sim as sim
 import lux_sim as sim
+# import lux_training_sim as sim
 import statistics
 import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 
 if __name__ == "__main__":
     simulator = sim.LuxSim(10, 1.0, 5, False)
+    nb_episodes = 300
     while simulator.step():
-        '''print(simulator.get_curr_nb_iterations())
+        '''print("iteration:", simulator.get_curr_nb_iterations())
         print(simulator.get_state())
         print(simulator.get_reward())'''
 
@@ -15,13 +16,33 @@ if __name__ == "__main__":
     averageHourlyWaitingTimes = [statistics.mean(x) for x in simulator.averageHourlyWaitingTimes]
     hours = [x + 1 for x in range(len(averageHourlyRewards))]
 
+    reward = statistics.mean(simulator.averageRewards)
+    waiting_time = statistics.mean(simulator.averageWaitingTimes)
+    stddev_r = statistics.stdev(simulator.averageRewards)
+    stddev_w = statistics.stdev(simulator.averageWaitingTimes)
+
+    print("Average reward:", reward)
+    print("Average waiting time:", waiting_time)
+    print("Reward standard deviation:", stddev_r)
+    print("Waiting time standard deviation:", stddev_w)
+
     tb = SummaryWriter(log_dir="runs/hourly_LuST_baseline")
+
+    tb.add_scalar("Average reward", reward, 1)
+    tb.add_scalar("Average waiting time", waiting_time, 1)
+    tb.add_scalar("Reward standard deviation", stddev_r, 1)
+    tb.add_scalar("Waiting time standard deviation", stddev_w, 1)
+    tb.add_scalar("Average reward", reward, nb_episodes)
+    tb.add_scalar("Average waiting time", waiting_time, nb_episodes)
+    tb.add_scalar("Reward standard deviation", stddev_r, nb_episodes)
+    tb.add_scalar("Waiting time standard deviation", stddev_w, nb_episodes)
+
     for i in range(len(hours)):
         tb.add_scalar("Average hourly reward", averageHourlyRewards[i], hours[i])
         tb.add_scalar("Average hourly waiting time", averageHourlyWaitingTimes[i], hours[i])
     tb.close()
 
-    plt.figure()
+    '''plt.figure()
     plt.grid()
     plt.plot(hours, averageHourlyRewards, color="r", label="fixed time")
     plt.xlabel("Hour of the day")
@@ -35,4 +56,4 @@ if __name__ == "__main__":
     plt.xlabel("Hour of the day")
     plt.ylabel("Average waiting time (s)")
     plt.legend()
-    plt.show()
+    plt.show()'''

run_buses.py
@@ -1,4 +1,4 @@
-from simulator import Simulator
+from priority_simulator import PrioritySimulator
 from DQN import Agent
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
@@ -22,8 +22,10 @@ def collect_transition(replay_buf, simu, act):
 
 
 # Fills the replay buffer with nb_samples samples using random actions
-def initialize_buffer(rp_buf, nb_samples, nb_act, nb_ep_steps, det_rate, min_phase, route_prob, hour_day, hourly_probs):
-    sim = Simulator(None, nb_ep_steps, det_rate, min_phase, route_prob, hour_day, False, hourly_probs)
+def initialize_buffer(rp_buf, nb_samples, nb_act, nb_ep_steps, det_rate, min_phase, route_prob, bus_freq_1, bus_freq_2,
+                      bus_freq_3, bus_dev, pr_factor, hour_day, hourly_probs):
+    sim = PrioritySimulator(None, nb_ep_steps, det_rate, min_phase, route_prob, bus_freq_1, bus_freq_2, bus_freq_3,
+                            bus_dev, pr_factor, hour_day, False, hourly_probs)
     for i in range(nb_samples):
         selected_action = select_random_action(nb_act)
         collect_transition(rp_buf, sim, selected_action)
@@ -31,10 +33,12 @@ def initialize_buffer(rp_buf, nb_samples, nb_act, nb_ep_steps, det_rate, min_pha
     print("END OF SIMULATION")
 
 
-def test_agent(sim, tb, nb_ep_test, nb_ep_steps, det_rate, min_phase, route_prob, hour_day, hourly_probs, ag):
+def test_agent(sim, tb, nb_ep_test, nb_ep_steps, det_rate, min_phase, route_prob, bus_freq_1, bus_freq_2, bus_freq_3,
+               bus_dev, pr_factor, hour_day, hourly_probs, ag):
     sim.close_simulation()
     print("\nENTERING TESTING PHASE")
-    test_sim = Simulator(nb_ep_test, nb_ep_steps, det_rate, min_phase, route_prob, hour_day, False, hourly_probs)
+    test_sim = PrioritySimulator(nb_ep_test, nb_ep_steps, det_rate, min_phase, route_prob, bus_freq_1, bus_freq_2,
+                                 bus_freq_3, bus_dev, pr_factor, hour_day, False, hourly_probs)
     while test_sim.step(ag.select_action(test_sim.get_state(), True)):
         pass
     av_r = statistics.mean(test_sim.averageRewards)
@@ -83,13 +87,13 @@ if __name__ == "__main__":
 
     mem_size = 100000
     nb_init = 10000  # Number of samples in the replay buffer before learning starts
-    nb_inputs = 11
+    nb_inputs = 15
     nb_actions = 2  # Either stay at current phase or switch to the next one
     nb_episodes = 200
     nb_episodes_test = 30
     nb_episodes_between_tests = 5
     nb_episode_steps = 3000
-    detection_rate = 0.5  # Percentage of vehicles that can be detected by the algorithm
+    detection_rate = 1.0  # Percentage of vehicles that can be detected by the algorithm
     min_phase_duration = 10
     gui = False
     alpha = 0.0001
@@ -105,8 +109,13 @@ if __name__ == "__main__":
     target_update_frequency = 3000
     hour_of_the_day = 8
     # Probability for a car to be generated on a particular route at a certain step
-    route_probabilities = [1. / 30] * 3 + [1. / 60] * 3 + [1. / 30] * 3 + [1. / 60] * 3
-    gen_name = "model_hor_30_60_50_RMS"
+    route_probabilities = [1. / 45] * 12
+    bus_frequency_1 = 600
+    bus_frequency_2 = 900
+    bus_frequency_3 = 600
+    bus_stddev = 90
+    priority_factor = 1
+    gen_name = "model_100_medium_buses"
     file_name = gen_name + ".pt"
     doTesting = True
 
@@ -120,11 +129,13 @@ if __name__ == "__main__":
                   batch_size, nb_inputs, nb_actions, mem_size, file_name)
     print("\nINITIALIZING REPLAY BUFFER")
     initialize_buffer(agent.replayBuffer, nb_init, nb_actions, nb_episode_steps, detection_rate, min_phase_duration,
-                      route_probabilities, hour_of_the_day, h_probs)
+                      route_probabilities, bus_frequency_1, bus_frequency_2, bus_frequency_3, bus_stddev,
+                      priority_factor, hour_of_the_day, h_probs)
     print("REPLAY BUFFER INITIALIZATION DONE\n")
     # /!\ Has to be initialized AFTER buffer initialization! initialize_buffer() uses its own simulator
-    simulator = Simulator(nb_episodes, nb_episode_steps, detection_rate, min_phase_duration, route_probabilities,
-                          hour_of_the_day, gui, h_probs)
+    simulator = PrioritySimulator(nb_episodes, nb_episode_steps, detection_rate, min_phase_duration,
+                                  route_probabilities, bus_frequency_1, bus_frequency_2, bus_frequency_3, bus_stddev,
+                                  priority_factor, hour_of_the_day, gui, h_probs)
 
     # Learning phase
     print("STARTING LEARNING")
@@ -133,14 +144,16 @@ if __name__ == "__main__":
         if doTesting and simulator.get_episode_end() == 1 and (
                 simulator.episodeCnt - 1) % nb_episodes_between_tests == 0:
             test_agent(simulator, writer, nb_episodes_test, nb_episode_steps, detection_rate, min_phase_duration,
-                       route_probabilities, hour_of_the_day, h_probs, agent)
+                       route_probabilities, bus_frequency_1, bus_frequency_2, bus_frequency_3, bus_stddev,
+                       priority_factor, hour_of_the_day, h_probs, agent)
         action = agent.select_action(simulator.get_state())
         continue_simulation = collect_transition(agent.replayBuffer, simulator, action)
         agent.learning_step()
         if simulator.currNbIterations % target_update_frequency == 0:
             agent.update_target_net()
     test_agent(simulator, writer, nb_episodes_test, nb_episode_steps, detection_rate, min_phase_duration,
-               route_probabilities, hour_of_the_day, h_probs, agent)
+               route_probabilities, bus_frequency_1, bus_frequency_2, bus_frequency_3, bus_stddev, priority_factor,
+               hour_of_the_day, h_probs, agent)
     print("LEARNING DONE")
 
     if doTesting: